e8c5714f2f0b2326bff6e9b357c2186eb4392c60
Ben Pollack <benpollack8723@gmail.com>
2024-07-26 15:28:19 -0500

    basic file setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
+const { route } = require("../../routes/todos.router");
+
 console.log('JS is sourced!');
+
+// GET route
+
+// POST route
+
+// PUT route
+
+// DELETE route
+
+// HELPER functions
+
+// EXTRA FEATURES functions
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+// GET route
 
+// POST route
+
+// PUT route
+
+//  DELETE route
 
 
 module.exports = router;
