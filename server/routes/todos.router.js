4c39bee20ecb511b899a77b6add294e9cfeaf5c6
Ben Pollack <benpollack8723@gmail.com>
2024-07-26 16:16:19 -0500

    update get route

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -2,7 +2,21 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 // GET route
-
+router.get('/', (req, res) => {
+    let queryText = `
+            SELECT * FROM "todos"
+                ORDER BY "priority";
+    `;
+    
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+      .catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+      });
+  });
 // POST route
 
 // PUT route
