80a4a585883d77038fc1133b142602d0ec4fcb32
Ben Pollack <benpollack8723@gmail.com>
2024-07-29 16:40:32 -0500

    server setup

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -5,12 +5,23 @@ let databaseName = 'weekend-to-do-app'
 if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
+let pool;
 
-const pool = new pg.Pool({
-  host: 'localhost',
-  port: 5432,
-  database: databaseName,
-  allowExitOnIdle: true
-})
+if (process.env.DATABASE_URL) {
+  
+  pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+          rejectUnauthorized: false
+      }
+  });
+} else {
+    pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: databaseName,
+    allowExitOnIdle: true 
+});
+}
 
 module.exports = pool
